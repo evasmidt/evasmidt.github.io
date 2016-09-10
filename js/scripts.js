64c05a21a10b01e6604093fa5a5689cc5ced2d76
evasmidt <shevangeline@gmail.com>
2016-09-10 17:57:23 -0400

    small changes to clean up code

--- js/scripts.js
+++ js/scripts.js
@@ -79,22 +79,7 @@ $('.js--nav-icon').click(function(){
 
 	});
 
-/*	$('.js--nav-icon').click(function(){
-		var nav = $('.js--main-nav');
-		var icon = $('.js--nav-icon i');
 
-		nav.slideToggle(200);
-
-		if (icon.hasClass('.fa-bars')) {
-		icon.addClass('.fa-times');
-		icon.removeClass('.fa-bars');
-		
-		} else {
-			icon.addClass('.fa-bars');
-			icon.removeClass('.fa-times');
-		}
-
-	});*/
 
 
 /*Scroll to Top of page for logo link*/
